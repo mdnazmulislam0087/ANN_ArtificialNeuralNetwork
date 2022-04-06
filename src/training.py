f272864ae0f6011000fb9c53b7ecf665771bcefd
MD NAZMUL ISLAM <md.nazmul.islam0087@gmail.com>
2022-04-06 23:52:20 +0600

    TRaining part added

--- src/training.py
+++ src/training.py
@@ -1,6 +1,7 @@
 import os
 from utils.common import read_config
 from utils.data_mgmt import get_data
+from utils.model import create_model
 
 import argparse
 
@@ -9,7 +10,19 @@ def training(config_path):
     #print(config)
     validation_datasize = config["params"]["validation_datasize"]
     (X_train, y_train), (X_valid, y_valid), (X_test, y_test) = get_data(validation_datasize)
-    print(X_train.shape)
+    #print(X_train.shape)
+    
+    LOSS_FUNCTION = config["params"]["loss_function"]
+    OPTIMIZER = config["params"]["optimizer"]
+    METRICS = config["params"]["metrics"]
+    NUM_CLASSES = config["params"]["num_classes"]
+
+    model = create_model(LOSS_FUNCTION, OPTIMIZER, METRICS, NUM_CLASSES)
+    
+    EPOCHS = config["params"]["epochs"]
+    VALIDATION = (X_valid, y_valid)
+
+    history = model.fit(X_train, y_train, epochs=EPOCHS, validation_data=VALIDATION)
     
     
     
